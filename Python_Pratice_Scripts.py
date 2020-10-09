ace11db6c61e0e91efa9ecd13a72db69f7d6d978
Thomas Serafin <thomas.serafin2@gmail.com>
2020-10-08 23:17:41 -0400

    kids in candy store

--- Python_Pratice_Scripts.py
+++ Python_Pratice_Scripts.py
@@ -15,3 +15,21 @@ nums = [1, 2, 3, 4] # test array
        
       return(list)
 # ------------------------------------------------------
+# Kids With the Greatest Number of Candies
+
+candies = [2,3,5,1,3]
+extraCandies = 3
+
+# two solutions 
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        return [ True if i + extraCandies >= max(candies) else False for i in candies ]
+    
+    
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]
+      for i in range(0, len(candies)):
+          if extraCandies + candies[i] >= max(candies):
+              list.append("True")
+          else:
+              list.append("False")
+      return(list)
+    
